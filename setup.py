138fa12889a93f546accf24b8f12003abd84c787
ThiagoPoppe <thiagopoppe@dcc.ufmg.br>
2022-02-10 10:06:49 -0300

    Adicionando arquivo setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from distutils.core import setup
+
+if __name__ == "__main__":
+    if sys.version_info[:2] < (3, 6):
+        print('Requires Python version 3.6 or later')
+        sys.exit(-1)
+
+    setup(name='snare_rythm_transcription',
+          install_requires=[line.strip() for line in open('requirements.txt')],
+          version='0.01',
+          description='Requerimentos para executar o projeto Snare Rythm Transcription da disciplina MIR do DCC/UFMG',
+          author='Thiago Martin Poppe')
