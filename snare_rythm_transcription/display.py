3050562e70ed8055c4ab555fe0e6a335eba85ebb
ThiagoPoppe <thiagopoppe@dcc.ufmg.br>
2022-02-11 11:32:43 -0300

    Todas as notas terão o stem para cima

--- snare_rythm_transcription/display.py
+++ snare_rythm_transcription/display.py
@@ -5,12 +5,26 @@ from midi2audio import FluidSynth
 from .exceptions import SoundFontNotFound
 
 def show(music):
+    """
+        Método para exibir a partitura da música.
+
+        Infelizmente o music21 ainda não suporta muitas features de instrumentos percussivos.
+        Para isso, iremos modificar a nota apenas para a sua exibição correta.
+        Por exemplo, um toque na caixa está na posição da nota B4 na partitura.
+    """
     music_cp = copy.deepcopy(music)
     for note in music_cp.recurse().notes:
         note.name = 'B4' # toques na caixa correspondem à posição do B4
+        note.stemDirection = 'up'
+
     music_cp.show()
 
 def play(music, wavname='music.wav', sound_font='pns_drum_kit.SF2'):
+    """
+        Método para tocarmos a música.
+        Como estamos trabalhando exclusivamente com percussão, utilizaremos
+        por padrão um SoundFont de bateria.
+    """
     if not os.path.exists(sound_font):
         raise SoundFontNotFound(sound_font)
 
